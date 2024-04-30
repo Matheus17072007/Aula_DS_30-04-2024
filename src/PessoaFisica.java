34ca4fe8804c1d4a74e6021db67c402b3e9d1fc3
Matheus Teixeira <MATHEUS_4086@uirapuru.br>
2024-04-30 09:51:18 -0300

    Initial commit

---
+++ src/PessoaFisica.java
@@ -0,0 +1,12 @@
+public class PessoaFisica extends Pessoa{
+
+    private String rg;
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public String getRg() {
+        return (rg);
+    }
+}
